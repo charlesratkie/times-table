9f94f7cf58fb5b12d40e19c27ff2075db4fb6dbd
charlesratkie <charlesratkie@gmail.com>
2015-10-15 13:37:10 -0700

    Create prog1_1.cpp

---
+++ prog1_1.cpp
@@ -0,0 +1,131 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+
+//consider implementing a time out notification if the user takes longer than 5 seconds.
+
+
+int main()
+{
+	// Seed the rand
+	//srand(time(0));
+
+
+	//array for the numbers
+
+	int numbers[13][13];
+
+	//User variables
+	char response;
+	int wrong_answer = 0;
+	int right_answer = 0;
+
+	//random number variables
+	int rand1 = 0;
+	int rand2 = 0;
+	int answer = 0;
+	int question = 0;
+
+	numbers[rand1][rand2];
+
+	// randomize both variables
+	//random_num_one = (rand()%12+1);
+	//random_num_two = (rand()%12+1);
+	//question = (random_num_one * random_num_two);
+
+	//points variable
+	int point_tracker = 0;
+
+
+
+
+
+	// Welcome the User to the game -- Explain the rules
+	cout << endl << endl << endl;
+	cout << "Welcome to the Times Table Challenge!\n\n";
+	cout << "You will be given a question (i.e 5 * 5) randomly from numbers 1 - 12.\n";
+	cout <<	"If you guess a question wrong but are within 5 of the answer, "
+	     << "you will get another chance.\n\n";
+	cout << "*If you get 3 wrong in a row. You will lose.\n" << endl;
+	cout << "*A point will be given for each right answer." << endl << endl;
+
+	// Ask the user if they would like to play
+	cout << "Would you like to play?";
+	cin >> response;
+	response = toupper(response);
+	cout << endl << endl;
+
+	if ( response == 'Y' )
+	{
+		cout << "LETS PLAY!\n\n";
+
+		while ( wrong_answer < 3)  // || right_answer < max_right_answer )
+		{
+
+		    // Seed the rand
+		    srand(time(0));
+		    rand1 = (rand()%12+1);
+		    rand2 = (rand()%12+1);
+
+		    question = (rand1 * rand2);
+		    // read out randomized variables and multiply together.
+		    cout << rand1 << " * " << rand2 << " = " ;
+		    // read in user's answer.
+		    cin >> answer;
+
+		    if ( answer == question )
+		    {
+			wrong_answer = 0;
+
+			point_tracker++;
+		    	cout << "You are correct!\n";
+			cout << "Points: " << point_tracker << endl;
+		    	// Add a point system to track # correct answers.
+		    }
+
+		    else
+		    {
+		    	// Add a counter to add to wrong_answer.
+			wrong_answer++;
+			cout << "Sorry, you are incorrect.\n\n";
+			cout << "Number of wrong answers: " << wrong_answer << endl;
+			cout << "Attempts remaing: " << 3 - wrong_answer <<  endl;
+			if ( wrong_answer == 3 )
+			{
+			    cout << "Sorry you were incorrect 3 times.\n\n\n";
+			    cout << "GAME OVER!" << endl;
+			}
+		    }
+                }
+	}
+
+	else
+	{
+		cout << "Awe, alright. Maybe next time!\n";
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	return 0;
+}
